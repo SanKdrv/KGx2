353fec762dfc0a8f9429a704456e282e7f126aba
Kabaan <romanfcb123@gmail.ru>
2024-11-28 14:47:16 +0300

    Открытие потока для парсера в main

--- main.py
+++ main.py
@@ -4,13 +4,24 @@ import asyncio
 from dotenv import find_dotenv, load_dotenv
 from aiogram import Bot, Dispatcher
 import os
+from parse.parse_module import Parse
+from models.models import *
+from config import Config
 
+# Инициализация config
+config = Config()
+
+# Инициализация ORM моделей
+users = Users('Users')
+tokens = Tokens('Tokens')
+users_tokens = UsersTokens('UsersTokens')
 
 # Инициализация модулей
 broker = message_broker_module.MessageBroker()
+parser = Parse(tokens_table=tokens, config=config)
 
 # Создание потоков
-# thread1 = threading.Thread(target=task_module_1)
+thread1 = threading.Thread(target=parser.parse_bybit())
 # thread2 = threading.Thread(target=task_module_2)
 # thread3 = threading.Thread(target=task_module_3)
 # thread4 = threading.Thread(target=task_module_4)
