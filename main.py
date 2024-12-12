573dba6bc1b92d33575a8e7e853b725292f923d1
Alexander Kudryashov <kudryash@cs.petrsu.ru>
2024-12-12 21:11:31 +0300

    Рефакторинг

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 import threading
-import time
 from aiogram.enums import ParseMode
 from aiogram.types import FSInputFile
 from matplotlib import pyplot as plt
 from matplotlib.dates import date2num, DateFormatter
-from pandas.core._numba import executor
 import pandas as pd
 from datetime import datetime
-import chatbot.chatbot_module
-from message_broker import message_broker_module
 import asyncio
 from aiogram import Bot, Dispatcher
-import config
-import os
 from parse import parse_module
 from models.models import *
 from config import *
@@ -26,7 +20,6 @@ tokens = Tokens('Tokens')
 users_tokens = UsersTokens('UsersTokens')
 
 # # Инициализация модулей
-# broker = message_broker_module.MessageBroker()
 parser = parse_module.Parse(tokens_table=tokens)
 alg = RSI_main.AlghorizmizationModule()
 
@@ -34,54 +27,13 @@ q = Queue()
 
 # # Создание потоков
 thread1 = threading.Thread(target=parser.parse_bybit)
-# thread2 = threading.Thread(target=alg.run)
-# thread3 = threading.Thread(target=task_module_3)
-# # thread4 = threading.Thread(target=task_module_4)
-# thread5 = threading.Thread(target=broker.run_broker)
-#
-# thread5.start()
 
 thread1.start()
-# thread2.start()
-# print(thread2.)
-# thread2.join()
-# thread2.result()
 
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
 dp = Dispatcher()
 
 
-# async def botStart():
-#     from chatbot.handlers.user_private import user_private_router
-#     # Подключение роутера
-#     dp.include_routers(user_private_router)
-#     await bot.delete_webhook(drop_pending_updates=True)
-#     task = asyncio.create_task(coro=scheduler(delay=15))
-#     # await scheduler(delay=15)
-#     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types(), skip_updates = True,)
-
-
-# async def scheduler(delay):
-#     while True:
-#         # print('jopa')
-#         # await chatbot.chatbot_module.pull_gueue()
-#         alg = RSI_main.AlghorizmizationModule()
-#         data = alg.process_data()
-#         tasks = []
-#         for item in data:
-#             tiker = item[0][:-4]
-#             print(tiker)
-#             token_UID = tokens.get_token_ID(item[0])
-#             s = f'https://www.bybit.com/ru-RU/trade/spot/{tiker}/USDT'
-#             users_id_list = users_tokens.get_users_by_token(int(token_UID))
-#             tasks += [bot.send_message(chat_id=user,
-#                                        text=f'Заносик, покупай здесь: https://www.bybit.com/ru-RU/trade/spot/{tiker}/USDT')
-#                       for user in users_id_list]
-#         # all_tasks = asyncio.all_tasks()
-#         # print(all_tasks)
-#         await asyncio.gather(*tasks)
-#         await asyncio.sleep(delay=delay)
-#         # time.sleep(10)
 async def botStart():
     from chatbot.handlers.user_private import user_private_router
 
@@ -172,6 +124,7 @@ def draw_chart(ticker_name: str, chart_data: list = None) -> str:
     plt.close(fig)
 
     return saved_name
+
 
 def parse_date(date_str):
     return datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
